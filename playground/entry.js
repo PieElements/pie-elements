57886e6f717b5021191d4413873ed8b486ecb540
edeustace <ed.eustace@gmail.com>
2017-08-03 10:12:00 +0100

    check that jss names are working

--- playground/entry.js
+++ playground/entry.js
@@ -1,21 +1,37 @@
 console.log('hi');
 
+import { MuiThemeProvider, createMuiTheme, createStyleSheet, withStyles } from 'material-ui/styles';
+
 import PropTypes from 'proptypes';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TextField from 'material-ui/TextField';
 
+const theme = createMuiTheme({});
+
 class Holder extends React.Component {
 
 
   render() {
-    const { value, onChange } = this.props;
-    return <div>
+    const { value, onChange, classes } = this.props;
+    return <div className={classes.root}>
       holder
       <TextField value={value} onChange={onChange} />
     </div>;
   }
 }
+
+const holderStyles = createStyleSheet('Holder', theme => {
+  return {
+    root: {
+      width: '100%'
+    }
+  }
+});
+
+
+const StyledHolder = withStyles(holderStyles)(Holder);
+
 
 class App extends React.Component {
 
@@ -32,9 +48,9 @@ class App extends React.Component {
   }
 
   render() {
-    return <div>
-      <Holder value={this.state.value} onChange={this.onChange} />
-    </div>;
+    return <MuiThemeProvider theme={theme}><div>
+      <StyledHolder value={this.state.value} onChange={this.onChange} />
+    </div></MuiThemeProvider>;
   }
 }
 
