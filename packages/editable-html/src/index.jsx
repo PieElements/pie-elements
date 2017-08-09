2d3d316fa2e39077290c22c5c2dab490867c53b9
edeustace <ed.eustace@gmail.com>
2017-08-09 16:53:53 +0100

    use editorState.getCurrentContent().hasText()

--- packages/editable-html/src/index.jsx
+++ packages/editable-html/src/index.jsx
@@ -19,16 +19,27 @@ const style = {
   }
 }
 
+const getMarkup = (editorContent) => {
+  const div = document.createElement('div');
+  div.innerHTML = stateToHTML(editorContent);
+  const out = div.firstChild.innerHTML;
+  return out;
+}
+
 export class EditableHTML extends React.Component {
 
   constructor(props) {
     super(props);
     const content = this.props.model === undefined ? '' : this.props.model;
+    const editorState = EditorState.createWithContent(stateFromHTML(content));
+
+    const currentContent = editorState.getCurrentContent();
+
     this.state = {
       showToolbar: false,
       active: false,
-      hasText: !isEmpty(content),
-      editorState: EditorState.createWithContent(stateFromHTML(content))
+      hasText: currentContent.hasText(),
+      editorState
     };
     this.onStyle = this.onStyle.bind(this);
     this.onChange = this.onChange.bind(this);
@@ -38,12 +49,10 @@ export class EditableHTML extends React.Component {
   }
 
   onChange(editorState) {
-    const content = stateToHTML(editorState.getCurrentContent());
-    this.setState({ editorState });
-    this.setState({
-      hasText: !isEmpty(content)
-    });
-    this.props.onChange(content);
+    const currentContent = editorState.getCurrentContent();
+    const markup = getMarkup(currentContent);
+    this.setState({ editorState, hasText: currentContent.hasText() });
+    this.props.onChange(markup);
   }
 
   handleKeyCommand(command) {
@@ -53,12 +62,17 @@ export class EditableHTML extends React.Component {
     }
   }
 
-  onEditorBlur() {
+  onEditorBlur(event) {
     this.blurTimeoutId = setTimeout(() => {
       this.setState({
         active: false
       });
-    }, 200);
+      this.blurTimeoutId = undefined;
+    }, 140);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.blurTimeoutId);
   }
 
   onStyle(style) {
@@ -83,31 +97,47 @@ export class EditableHTML extends React.Component {
   }
 
   render() {
-    const { classes } = this.props;
-    return (<div>{
-      this.state.active === true ? (
-        <div className={classes.root}>
-          <Toolbar
-            onStyle={this.onStyle} />
-          <div className={classes.editor}>
-            <Editor
-              onBlur={this.onEditorBlur}
-              editorState={this.state.editorState}
-              handleKeyCommand={this.handleKeyCommand}
-              onChange={this.onChange} />
-          </div>
-        </div>
-      ) : (
-          this.state.hasText ? (
-            <div dangerouslySetInnerHTML={{ __html: stateToHTML(this.state.editorState.getCurrentContent()) }}
-              onClick={this.toggleHtml}></div>
-          ) : (
-              <div onClick={this.toggleHtml}>{this.props.placeholder}</div>
-            )
-        )
-    }</div>);
+    const { classes, placeholder, className } = this.props;
+    const { editorState, hasText } = this.state;
+    return (
+      <div className={className}>{
+        this.state.active ?
+          <Active
+            classes={classes}
+            onStyle={this.onStyle}
+            onBlur={this.onEditorBlur}
+            editorState={this.state.editorState}
+            handleKeyCommand={this.handleKeyCommand}
+            onChange={this.onChange} /> :
+          <Preview
+            hasText={hasText}
+            placeholder={placeholder}
+            markup={() => getMarkup(editorState.getCurrentContent())}
+            onClick={this.toggleHtml} />}
+      </div>
+    );
   }
-
 }
+
+const Preview = ({ onClick, hasText, markup, placeholder }) => {
+  const html = hasText ? markup() : placeholder;
+  return <div
+    onClick={onClick}
+    dangerouslySetInnerHTML={{ __html: html }}></div>;
+};
+
+const Active = ({ classes, onStyle, onBlur, editorState, handleKeyCommand, onChange }) => (
+  <div className={classes.root}>
+    <Toolbar
+      onStyle={onStyle} />
+    <div className={classes.editor}>
+      <Editor
+        onBlur={onBlur}
+        editorState={editorState}
+        handleKeyCommand={handleKeyCommand}
+        onChange={onChange} />
+    </div>
+  </div>
+)
 
 export default injectSheet(style)(EditableHTML);
