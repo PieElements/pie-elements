20038760f8cdd4e241e97a26d235f900955e2573
edeustace <ed.eustace@gmail.com>
2017-08-14 19:05:08 +0100

    feat(ui): add config-ui package

---
+++ packages/config-ui/src/index.js
@@ -0,0 +1,9 @@
+import Langs from './langs';
+import MultiLangInput from './multi-lang-input';
+import NumberTextField from './number-text-field';
+
+export {
+  Langs,
+  MultiLangInput,
+  NumberTextField
+}
