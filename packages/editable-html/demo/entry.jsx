b1ac646b51f6d86ab83638e2bf1004a52deb3b60
edeustace <ed.eustace@gmail.com>
2017-08-16 23:01:18 +0100

    look at image upload and removal

--- packages/editable-html/demo/entry.jsx
+++ packages/editable-html/demo/entry.jsx
@@ -1,8 +1,83 @@
-import CustomImageEditor from './custom-image-editor';
+import {
+  ContentState,
+  EditorState,
+  convertFromHTML,
+  convertFromRaw,
+  convertToRaw
+} from 'draft-js';
+
+// import CustomImageEditor from './custom-image-editor';
 import EditableHtml from '../src/index';
+import MediaEditor from './media-editor';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { RichEditorExample } from './example';
+import { stateToHTML } from 'draft-js-export-html';
+
+const LogState = ({ editorState }) => {
+  return <pre>{stateToHTML(editorState.getCurrentContent())}</pre>
+}
+const blocksFromHtml = convertFromHTML('<div>hi</div>');
+
+const initialState = ContentState.createFromBlockArray(
+  blocksFromHtml.contentBlocks,
+  blocksFromHtml.entityMap
+)
+console.log('initial state: ', initialState);
+// const initialState = {
+//   entityMap: {
+//     "0": {
+//       "type": "image",
+//       "mutability": "IMMUTABLE",
+//       "data": {
+//         "src": "/images/canada-landscape-small.jpg"
+//       }
+//     }
+//   },
+//   blocks: [{}]
+// };
+
+const initialStateOrig = {
+  "entityMap": {
+    "0": {
+      "type": "image",
+      "mutability": "IMMUTABLE",
+      "data": {
+        "src": "/images/canada-landscape-small.jpg"
+      }
+    }
+  },
+  "blocks": [{
+    "key": "9gm3s",
+    "text": "You can have images in your text field. This is a very rudimentary example, but you can enhance the image plugin with resizing, focus or alignment plugins.",
+    "type": "unstyled",
+    "depth": 0,
+    "inlineStyleRanges": [],
+    "entityRanges": [],
+    "data": {}
+  }, {
+    "key": "ov7r",
+    "text": " ",
+    "type": "atomic",
+    "depth": 0,
+    "inlineStyleRanges": [],
+    "entityRanges": [{
+      "offset": 0,
+      "length": 1,
+      "key": 0
+    }],
+    "data": {}
+  }, {
+    "key": "e23a8",
+    "text": "See advanced examples further down …",
+    "type": "unstyled",
+    "depth": 0,
+    "inlineStyleRanges": [],
+    "entityRanges": [],
+    "data": {}
+  }]
+};
+/* eslint-enable */
 
 class Demo extends React.Component {
 
@@ -10,17 +85,26 @@ class Demo extends React.Component {
     super(props);
     this.onImageClick = this.onImageClick.bind(this);
     this.handleFileSelect = this.handleFileSelect.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.state = {
+      // editorState: EditorState.createWithContent(initialState),
+      // editorState: EditorState.createWithContent(convertFromRaw(initialStateOrig)),
+    }
   }
 
   handleFileSelect(event) {
+    this.fileInput.blur();
     console.log('files: ', event);
     const file = event.target.files[0];
+    this.fileInput.value = '';
     var reader = new FileReader();
-    reader.onload = function () {
+    reader.onload = () => {
       const dataURL = reader.result;
-      console.log('dataUrl: ', dataURL);
+      console.log('call setHref...');
+      this.state.setHref(dataURL);
+      this.setState({ setHref: null });
     };
-
+    console.log('call readAsDataUrl...', file);
     reader.readAsDataURL(file);
   }
 
@@ -32,14 +116,23 @@ class Demo extends React.Component {
     this.fileInput.removeEventListener('change', this.handleFileSelect);
   }
 
-  onImageClick() {
+  onImageClick(setHref) {
+    this.setState({ setHref });
     this.fileInput.click();
+  }
+
+  onChange(editorState) {
+    console.log('update state: ...');
+    this.setState({ editorState });
   }
 
   render() {
     const { us } = this.props;
     return <div>
-      <CustomImageEditor />
+      <input ref={r => this.fileInput = r} type="file" hidden></input>
+      <MediaEditor addImage={this.onImageClick} />
+      {/* <CustomImageEditor editorState={this.state.editorState} onChange={this.onChange} /> */}
+      {/* <LogState editorState={this.state.editorState} /> */}
       {/* Us:
     <input ref={r => this.fileInput = r} type="file" hidden></input>
       <EditableHtml {...us} onImageClick={this.onImageClick} />
--- packages/editable-html/demo/webpack.config.js
+++ packages/editable-html/demo/webpack.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  devtool: 'cheap-eval-source-map',
   context: __dirname,
   entry: './entry.jsx',
   output: {
