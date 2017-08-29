f54aafa48126e439d8bf2c5396fa0305cd4dcdd3
edeustace <ed.eustace@gmail.com>
2017-08-29 11:19:48 +0100

    make event.detail the handler object

--- packages/multiple-choice/configure/src/index.js
+++ packages/multiple-choice/configure/src/index.js
@@ -7,31 +7,20 @@ import merge from 'lodash/merge';
 const log = debug('multiple-choice:configure');
 
 export class ModelUpdatedEvent extends CustomEvent {
-  constructor(m) {
-    super('model.updated', {
-      bubbles: true,
-      detail: {
-        update: m
-      }
-    });
+  constructor(update) {
+    super('model.updated', { bubbles: true, detail: { update } });
   }
 }
 
 export class DeleteImageEvent extends CustomEvent {
-  constructor(src, done) {
-    super('delete.image', {
-      bubbles: true,
-      detail: { src, done }
-    });
+  constructor(detail) {
+    super('delete.image', { bubbles: true, detail });
   }
 }
 
 export class InsertImageEvent extends CustomEvent {
-  constructor(handler) {
-    super('insert.image', {
-      bubbles: true,
-      detail: { handler }
-    });
+  constructor(detail) {
+    super('insert.image', { bubbles: true, detail });
   }
 }
 
@@ -65,7 +54,7 @@ export default class extends HTMLElement {
   }
 
   onDeleteImage(src, done) {
-    this.dispatchEvent(new DeleteImageEvent(src, done));
+    this.dispatchEvent(new DeleteImageEvent({ src, done }));
   }
 
   _render() {
