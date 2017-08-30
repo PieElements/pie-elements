a04939219f47d89a773496cb59d953cfd5a65bb6
edeustace <ed.eustace@gmail.com>
2017-08-30 21:41:55 +0100

    feat(feedback-config): add feedback config component

---
+++ packages/config-ui/demo/feedback-config/webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  context: __dirname,
+  entry: './entry.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        options: {
+          babelrc: false,
+          presets: [
+            require('babel-preset-react'), require('babel-preset-es2015')
+          ]
+        }
+      }
+    ]
+  },
+  resolve: {
+    symlinks: false,
+    extensions: ['.js', '.jsx']
+  }
+}
