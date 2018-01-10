c7368b2f869c72d4c3a3bd396549ab70a0b34249
edeustace <ed.eustace@gmail.com>
2018-01-10 14:50:51 -0500

    configure (wip)

--- packages/text-entry/configure/src/configure.jsx
+++ packages/text-entry/configure/src/configure.jsx
@@ -2,17 +2,48 @@ import { withStyles } from 'material-ui/styles';
 import React from 'react';
 import { Typography, TextField } from 'material-ui';
 import { Checkbox, FeedbackConfig } from '@pie-libs/config-ui';
+import Input, { InputLabel, InputAdornment } from 'material-ui/Input';
+
+const Box = withStyles(theme => ({
+  box: {
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2
+  }
+}))(({ classes, children }) => (<div className={classes.box}>{children}</div>))
+
+const SubHeader = withStyles(theme => ({
+  subHeader: {
+    paddingTop: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    fontSize: '0.7rem'
+  }
+}))(({ classes, children }) => <Typography className={classes.subHeader}>{children}</Typography>);
+
+
+const Choice = withStyles(theme => ({
+  choice: {
+    padding: theme.spacing.unit,
+    border: 'solid 1px grey'
+  }
+}))(({ classes, label }) => (
+  <span className={classes.choice}>{label}-----------------</span>
+));
+
+const Answers = ({ choices }) => (
+  <Input startAdornment={<InputAdornment>{choices.map(c => <Choice label={c} />)}</InputAdornment>} />
+)
 
 const RawResponses = ({ label, subHeader, children, classes }) => (
-  <div className={classes.responses}>
+  <Box>
     <Typography type="display">{label}</Typography>
-    <Typography className={classes.subHeader}>{subHeader}</Typography>
-    <div>answers here</div>
+    <SubHeader className={classes.subHeader}>{subHeader}</SubHeader>
+    <Answers choices={['a', 'b', 'c', 'd', 'e']} />
     <Checkbox label="Case Sensitive" />
     <Checkbox label="Ignore Whitespace" />
     {children ? children : <div />}
-  </div>
+  </Box>
 );
+
 const Responses = withStyles(theme => ({
   responses: {
     paddingTop: theme.spacing.unit,
@@ -37,13 +68,13 @@ const NumberOpts = () => (
 )
 
 const Constraints = ({ allowNumbers }) => (
-  <div>
-    <Typography>Options</Typography>
+  <Box>
+    <Typography type="display">Options</Typography>
     <Checkbox label="Only allow numbers" />
     {allowNumbers && <NumberOpts />}
     <InputSize />
     <InputAlignment />
-  </div>
+  </Box>
 )
 
 const styles = theme => ({
