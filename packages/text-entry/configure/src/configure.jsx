ba7e07b91d94a2d39bb6b065b452cc64235c11a6
edeustace <ed.eustace@gmail.com>
2018-01-11 14:54:53 -0500

    flesh out config panel

--- packages/text-entry/configure/src/configure.jsx
+++ packages/text-entry/configure/src/configure.jsx
@@ -1,8 +1,14 @@
 import { withStyles } from 'material-ui/styles';
 import React from 'react';
-import { Typography, TextField } from 'material-ui';
+import { Typography, TextField, FormControl, Select, MenuItem } from 'material-ui';
 import { Checkbox, FeedbackConfig, TagsInput } from '@pie-libs/config-ui';
 import Input, { InputLabel, InputAdornment } from 'material-ui/Input';
+import groupBy from 'lodash/groupBy';
+import { Cancel, AddCircle } from 'material-ui-icons';
+import IconButton from 'material-ui/IconButton';
+import debug from 'debug';
+
+const log = debug('pie-elements:text-entry:configure');
 
 const Box = withStyles(theme => ({
   box: {
@@ -24,18 +30,96 @@ const Answers = ({ choices, onChange }) => (
   <TagsInput tags={choices} onChange={onChange} />
 )
 
-const RawResponses = ({ label, subHeader, children, classes, onResponsesChange }) => (
-  <Box>
-    <Typography type="body1">{label}</Typography>
-    <SubHeader className={classes.subHeader}>{subHeader}</SubHeader>
-    <Answers choices={['a', 'b', 'c', 'd', 'e']} onChange={onResponsesChange} />
-    <Checkbox label="Case Sensitive" checked={false} />
-    <Checkbox label="Ignore Whitespace" checked={false} />
-    {children ? children : <div />}
-  </Box >
-);
+const Bit = withStyles(theme => ({
+  bit: {
+
+    padding: 0,//theme.spacing.unit,
+    paddingLeft: 0
+  },
+  contents: {
+    paddingTop: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit
+  }
+}))(({ classes, label, children }) => (
+  <td className={classes.bit}>
+    <div className={classes.contents}>{children}</div>
+  </td>
+));
+
+const Response = withStyles(theme => ({
+  response: {
+    width: '100%',
+  }
+}))(({ classes, value, lang, feedback, onDelete }) => (
+  <tr className={classes.response}>
+    <Bit label="value">{value}</Bit>
+    <Bit label="lang">{lang}</Bit>
+    <Bit label="feedback">{!feedback ? 'none' : (feedback === 'DEFAULT' ? 'default' : feedback)}</Bit>
+    <td><IconButton onClick={() => onDelete(value, lang)}><Cancel /></IconButton></td>
+  </tr>
+));
+
+class AddResponse extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      addLang: 'en-US'
+    }
+  }
+
+  render() {
+    const { onAddResponse } = this.props;
+
+    return (<tr>
+      <td><Input value={this.state.value} onChange={value => this.setState({ value })} /></td>
+      <td><Select value={this.state.addLang}>
+        <MenuItem value={'en-US'}>en-US</MenuItem>
+        <MenuItem value={'es-ES'}>es-ES</MenuItem>
+      </Select></td>
+      <td>todo</td>
+      <td><IconButton onClick={onAddResponse}><AddCircle /></IconButton></td>
+    </tr >
+    )
+  }
+}
+const RawResponses = ({ responses, label, subHeader, children, classes, onDelete, onAdd }) => {
+
+  return (
+    <Box>
+      <Typography type="body1">{label}</Typography>
+      <table className={classes.responsesGrid}>
+        <tbody>
+          <tr>
+            <th>value</th>
+            <th>lang</th>
+            <th>feedback</th>
+            <th></th>
+          </tr>
+          {(responses.values || []).map((r, index) => <Response
+            key={index}
+            {...r}
+            onDelete={onDelete} />)}
+
+          <AddResponse onAddResponse={onAdd} />
+        </tbody>
+      </table>
+      <Checkbox label="Case Sensitive" checked={false} />
+      <Checkbox label="Ignore Whitespace" checked={false} />
+      {children ? children : <div />}
+    </Box >
+  );
+}
 
 const Responses = withStyles(theme => ({
+  responsesGrid: {
+    width: '100%',
+    '& th': {
+      textAlign: 'left',
+      fontSize: '12px',
+      color: theme.palette.grey.A400
+    }
+  },
   responses: {
     paddingTop: theme.spacing.unit,
     paddingBottom: theme.spacing.unit
@@ -75,8 +159,21 @@ const styles = theme => ({
 });
 
 class Configure extends React.Component {
+
+  onDeleteCorrectResponse = (value, lang) => {
+    const { model } = this.props;
+    const update = model.correctResponses.values.filter(o => o.value !== value || o.lang !== lang);
+    log('[onDeleteCorrectResponse] update: ', update);
+    model.correctResponses.values = update;
+    this.props.onModelChanged(model);
+  }
+
+  onAddCorrectResponse = (value, lang) => {
+    //todo..
+  }
+
   render() {
-    const { classes } = this.props;
+    const { classes, model } = this.props;
 
     return (
       <div>
@@ -84,10 +181,14 @@ class Configure extends React.Component {
         <Responses
           label="Correct Answers"
           subHeader="Additional correct answers may be added by clicking enter/return between answers."
+          responses={model.correctResponses}
+          onDelete={this.onDeleteCorrectResponse}
+          onAdd={this.onAddCorrectResponse}
         />
         <Responses
           label="Partial Correct Answers (optional)"
-          subHeader="Additional partially correct answers may be added by clicking enter/return between answers." >
+          subHeader="Additional partially correct answers may be added by clicking enter/return between answers."
+          responses={[]}>
           <div>
             <TextField className={classes.award} placeholder="0" label="Award % for partially correct answer" />
           </div>
@@ -100,4 +201,29 @@ class Configure extends React.Component {
   }
 }
 
-export default withStyles(styles)(Configure);
+const ConfigureMain = withStyles(styles)(Configure);
+
+
+class StateWrapper extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      model: props.model
+    }
+
+    this.onModelChanged = (m) => {
+      this.setState({ model: m }, () => {
+        this.props.onModelChanged(this.state.model);
+      });
+    }
+  }
+
+
+  render() {
+    const { model } = this.state;
+    return <ConfigureMain model={model} onModelChanged={this.onModelChanged} />
+  }
+}
+
+export default StateWrapper;
--- packages/text-entry/configure/src/index.js
+++ packages/text-entry/configure/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Configure from './configure';
-
+import { ModelUpdatedEvent } from '@pie-libs/pie-configure-events';
 export default class TextEntryConfigure extends HTMLElement {
 
   constructor() {
@@ -13,10 +13,15 @@ export default class TextEntryConfigure extends HTMLElement {
     this._render();
   }
 
+  onModelChanged(model) {
+    this._model = model;
+    this.dispatchEvent(new ModelUpdatedEvent(this._model));
+  }
+
   _render() {
     if (this._model) {
       const el = React.createElement(Configure, {
-        onModelChanged: this.onModelChanged,
+        onModelChanged: this.onModelChanged.bind(this),
         model: this._model
       });
       ReactDOM.render(el, this);
