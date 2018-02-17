fd8c2de17ecf3abb500251b67baf4f1468d5b72d
edeustace <ed.eustace@gmail.com>
2018-02-16 21:05:00 -0500

    fix up use of ChoiceConfiguration

--- packages/inline-choice/configure/src/main.jsx
+++ packages/inline-choice/configure/src/main.jsx
@@ -1,59 +1,66 @@
 import React from "react";
 import { ChoiceConfiguration, MultiLangInput } from '@pie-libs/config-ui';
 import Button from 'material-ui/Button';
+import * as flattener from './flattener';
+import { withStyles } from 'material-ui/styles';
+
+const Choice = withStyles(theme => ({
+  choice: {
+    paddingTop: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit * 2
+  }
+}))(({ choice, onChange, onDelete, classes }) => (
+  <ChoiceConfiguration
+    className={classes.choice}
+    index={undefined}
+    mode={'radio'}
+    data={choice}
+    defaultFeedback={{
+      correct: 'Correct',
+      incorrect: 'Incorrect'
+    }}
+    onChange={onChange}
+    onDelete={onDelete} />
+));
 
 export default class Main extends React.Component {
 
-  constructor (props){
-    super (props);
+  constructor(props) {
+    super(props);
 
     this.state = {
       activeLang: props.model.defaultLang
     }
   }
 
-  render (){
+  onChoiceChange = (index, choice) => {
+    const update = flattener.expand(choice);
+    this.props.onChoiceChange(index, update);
+  }
 
-    let renderEditPrompt = () => {
-      return (
-        <MultiLangInput
-          label="Prompt"
-          value={this.props.model.prompt}
-          lang={this.state.activeLang}
-          onChange={this.props.onPromptUpdate}
-        />
-      );
-    }
+  render() {
 
-    let renderChoices = () => {
-      return this.props.model.choices.map((choice, index) => {
-        return <div key={index}>
-          <ChoiceConfiguration
-            mode={'radio'}
-            data={choice}
-            defaultFeedback={{
-              correct: 'Correct',
-              incorrect: 'Incorrect'
-            }}
-            onChange={(changeObj) => {this.props.onChoiceChange(index, changeObj)}}
-            onDelete={() => {this.props.onRemoveChoice(index)}}
-        /></div>
-      })
-    }
-
+    const usEnglishChoices = this.props.model.choices.map(flattener.flatten);
 
     return (
       <div>
-        {this.props.model.prompt &&
-        <div>
-          {renderEditPrompt()}
-          <br />
-          {renderChoices()}
-          </div>}
-        <br />
+        {this.props.model.prompt && (
+          <MultiLangInput
+            label="Prompt"
+            value={this.props.model.prompt}
+            lang={this.state.activeLang}
+            onChange={this.props.onPromptUpdate} />
+        )}
+        {usEnglishChoices.map((choice, index) => (
+          <Choice
+            choice={choice}
+            onChange={(choice) => this.onChoiceChange(index, choice)}
+            onDelete={() => this.onDelete(index)}
+            key={index} />
+        ))}
         <Button
           color="primary"
-          onClick={() => this.props.onAddChoice()} >Add a choice</Button>
+          onClick={() => this.props.onAddChoice()}>Add a choice</Button>
       </div>
     );
   }
--- packages/inline-choice/configure/src/root.jsx
+++ packages/inline-choice/configure/src/root.jsx
@@ -3,7 +3,7 @@ import Main from "./main";
 import cloneDeep from 'lodash/cloneDeep';
 
 export default class Root extends React.Component {
-  constructor (props){
+  constructor(props) {
     super(props);
     this.state = {
       model: props.model
@@ -15,43 +15,34 @@ export default class Root extends React.Component {
   }
 
   updateModel(model) {
-    this.setState({model}, () => {
+    this.setState({ model }, () => {
       this.handleModelChange();
     })
   }
 
-  onAddChoice () {
+  onAddChoice() {
     let updateModel = cloneDeep(this.state.model);
-    updateModel.choices.push({correct: false, value: "", feedback: {},label: ""});
+    updateModel.choices.push({ correct: false, value: "", feedback: {}, label: "" });
     this.updateModel(updateModel);
   }
 
   onUpdateChoiceFields(index, choice, type) {
     let updateModel = cloneDeep(this.state.model);
-    if(type === "label") {
+    if (type === "label") {
       updateModel.choices[index].label = choice;
-    }else if(type === "value") {
+    } else if (type === "value") {
       updateModel.choices[index].value = choice;
     }
     this.updateModel(updateModel);
   }
 
   onChoiceChange(index, newChoice) {
-    let updateModel = cloneDeep(this.state.model);
-console.log("newChoice", newChoice);
-    let updatedChoices = updateModel.choices.map((choice, choiceIndex) => {
-      if(newChoice.checked) {
-        if (index === choiceIndex) {
-          choice.checked = true;
-        } else {
-          choice.checked = false;
-        }
-      }
-      return choice;
-    });
-
-    updateModel.choices = updatedChoices;
-    this.updateModel(updateModel);
+    const update = cloneDeep(this.state.model);
+    if (newChoice.correct) {
+      update.choices.forEach(c => c.correct = false);
+    }
+    update.choices.splice(index, 1, newChoice);
+    this.updateModel(update);
   }
 
   onRemoveChoice(indexToRemove) {
@@ -67,14 +58,14 @@ console.log("newChoice", newChoice);
     this.updateModel(updateModel);
   }
 
-  render (){
+  render() {
     const props = {
       ...this.props,
       ...this.state,
-      onPromptUpdate : (prompt) => {return this.onUpdatePrompt(prompt)},
-      onChoiceChange : (index, newChoice) => {return this.onChoiceChange(index, newChoice)},
-      onRemoveChoice : (index) => { return this.onRemoveChoice(index)},
-      onAddChoice : () => {return this.onAddChoice()},
+      onPromptUpdate: (prompt) => { return this.onUpdatePrompt(prompt) },
+      onChoiceChange: (index, newChoice) => { return this.onChoiceChange(index, newChoice) },
+      onRemoveChoice: (index) => { return this.onRemoveChoice(index) },
+      onAddChoice: () => { return this.onAddChoice() },
     }
 
     return (
