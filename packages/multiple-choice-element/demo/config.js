3ab048db428db05237b4e4e64729f739aac9eb87
edeustace <ed.eustace@gmail.com>
2018-02-22 15:03:38 -0500

    add missing properties

--- packages/multiple-choice-element/demo/config.js
+++ packages/multiple-choice-element/demo/config.js
@@ -6,9 +6,14 @@ module.exports = {
     {
       element: 'multiple-choice',
       id: '1',
+      mode: 'gather',
       prompt: 'hi there',
+      disabled: false,
+      choiceMode: 'radio',
+      keyMode: 'letters',
       choices: [
-        { label: 'apple', value: 'a' }
+        { label: 'apple', value: 'a' },
+        { label: 'banana', value: 'b' }
       ]
     }
   ]
