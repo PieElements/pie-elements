5b7e66f6c17c8109d712ce5f0608fe0185cdca53
edeustace <ed.eustace@gmail.com>
2018-02-22 12:42:10 -0500

    chore(demo): add demo

---
+++ packages/multiple-choice-element/demo/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  elements: {
+    'multiple-choice': '..'
+  },
+  models: [
+    {
+      element: 'multiple-choice',
+      id: '1',
+      prompt: 'hi there',
+      choices: [
+        { label: 'apple', value: 'a' }
+      ]
+    }
+  ]
+}
