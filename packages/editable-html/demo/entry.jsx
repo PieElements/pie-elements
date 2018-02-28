01c76070d1a1891fc933e381707c0a903674b9fd
edeustace <ed.eustace@gmail.com>
2018-02-28 11:49:22 -0500

    feat(rename): rename packages
    BREAKING CHANGE: *-element packages have been removed

--- packages/editable-html/demo/entry.jsx
+++
@@ -1,8 +0,0 @@
-import Demo from './rte-demo';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const el = React.createElement(Demo, {});
-  ReactDOM.render(el, document.querySelector('#app'));
-});
--- packages/render-ui/src/index.js
+++
@@ -1,7 +0,0 @@
-import * as indicators from './response-indicators.jsx';
-import Feedback from './feedback';
-
-export {
-  indicators,
-  Feedback
-}
